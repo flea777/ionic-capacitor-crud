05e88a7dc535d8eb62e7fabc6c5ba94544c2340f
pedro <pedrodinizz2@outlook.com>
2023-08-31 10:17:17 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'crud-exercicio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
